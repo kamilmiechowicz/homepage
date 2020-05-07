7172c986ea2fd3dbda513a40aeb547a84425d2fd
Kamil Miechowicz <kmiechowicz123@gmail.com>
2020-05-07 10:12:36 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const name = prompt("Podaj swoje imie", ".....");
+if (name) {
+  alert("Witaj" + name);
+} else {
+  alert("Przykro mi że nie chcesz podaj swojego imienia :(");
+}
+
+console.log("zajebał ci ktoś kiedyś gonga gówniarzu?????");
